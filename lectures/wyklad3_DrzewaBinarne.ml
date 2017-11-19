1ee6c66a6c07016fa4296536e1ecd8a4cc911423
shvchuk <andrzej.szewczuk@gmail.com>
2017-11-19 03:19:35 +0100

    drzewa binarne def

--- lectures/wyklad3_DrzewaBinarne.ml
+++ lectures/wyklad3_DrzewaBinarne.ml
@@ -1,1 +1,9 @@
-(* definicja  *)
+(* definicja typu dla drzew binarnych jest rekurencyjna *)
+
+type 'a bt = Empty | Node of 'a * 'a bt * 'a bt;;
+
+let t = Node(1,Node(2,Empty,Node(3,Empty,Empty)),Empty);;
+
+let rec nodes = function Empty -> 0 | Node(_,t1,t2) -> 1 + nodes t1 + nodes t2;;
+
+nodes t;;
