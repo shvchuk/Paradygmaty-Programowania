c85e64dc081ef2b86d541b9a769707aa27e8aefb
shvchuk <andrzej.szewczuk@gmail.com>
2017-11-22 23:40:57 +0100

    binary trees structure

--- lectures/wyklad3_DrzewaBinarne.ml
+++ lectures/wyklad3_DrzewaBinarne.ml
@@ -7,3 +7,36 @@ let t = Node(1,Node(2,Empty,Node(3,Empty,Empty)),Empty);;
 let rec nodes = function Empty -> 0 | Node(_,t1,t2) -> 1 + nodes t1 + nodes t2;;
 
 nodes t;;
+
+(* przykład drzewa binarnego *)
+
+let tt = Node(1,
+              Node(2, 
+                Node(4,
+                    Empty,
+                    Empty
+                    ),
+                Empty
+                ),
+              Node(3,
+                  Node(5,
+                      Empty,
+                      Node(6,
+                          Empty,
+                          Empty
+                          )
+                      ),
+                  Empty      
+                  )
+              )
+;;
+
+nodes tt;;
+
+(* obejście drzew binarnych w głąb prefiksowo *)
+let rec preorder = function
+  Node (v,l,r) -> v::(preorder l) @ (preorder r)
+  | Empty -> []
+;;
+
+preorder tt;;
